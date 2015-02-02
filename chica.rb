dfab0c04f045d959fc50bad3f9a0a42e1654c7e5
abdulkadiryaman <abdulkadiryaman@gmail>
2015-02-02 20:06:10 +0200

    adding all related services

--- chica.rb
+++ chica.rb
@@ -14,7 +14,22 @@ light = Blinky.new.light
 when_run do |state|
   state[:servers]["go"].delete_if { |v|
     !(v.include? "Storefront.Web" or
-      v.include? "ProductInformation")
+        v.include? "Storefront.Journey.Tests" or
+        v.include? "Storefront.Regression.Tests" or
+        v.include? "Storefront.Performance.Tests" or
+        v.include? "Storefront.MobilePerformance.Tests" or
+        v.include? "ProductSearch.Service" or
+        v.include? "ProductInformation.Service" or
+        v.include? "Campaigns.Service" or
+        v.include? "Categories.Service" or
+        v.include? "Recommendations.Service" or
+        v.include? "ProductReviews.Service" or
+        v.include? "ContentManagement.Service" or
+        v.include? "ContentManagementAdmin.Service" or
+        v.include? "User.Service" or
+        v.include? "Storefront.Cache" or
+        v.include? "DeployAllStorefront" or
+        v.include? "BuildAndDeliver")
     }
   if state.has_failure?
     light.failure!
